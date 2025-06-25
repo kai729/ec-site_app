98aa78a53012fffc643633ef4d4ef581234652ba
kaito <kaito>
2025-06-25 16:58:44 +0900

    commit07

--- src/test-utils/CartProviderWrapper.tsx
+++
@@ -1,8 +0,0 @@
-// src/test-utils/CartProviderWrapper.tsx
-import React from "react";
-import { ReactNode } from "react";
-import { CartProvider } from "../contexts/CartContext";
-
-export const CartProviderWrapper = ({ children }: { children: ReactNode }) => {
-  return <CartProvider>{children}</CartProvider>;
-};
